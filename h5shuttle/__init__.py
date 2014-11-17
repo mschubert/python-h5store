2988ad3da0d0839bd63afbda3dc5c66c367e770a
mschubert <mschu.dev@gmail.com>
2014-11-17 21:55:26 +0000

    python: add save function

--- h5shuttle/__init__.py
+++ h5shuttle/__init__.py
@@ -1,5 +1,7 @@
 import h5py
 import pandas as pd
+# alternative: use PyTables format, write R wrapper so it can read them?
+# this would be quite a bit of work still..
 
 class _H5Node(dict):
     def __init__(self, kwargs):
@@ -7,6 +9,24 @@ class _H5Node(dict):
             setattr(self, k, v)
         self.update(kwargs)
 
+def save(X, file):
+    def value2path(file, path, node):
+        if (type(node) == "dict"):
+            file.create_group(path)
+            for k,v in node.iteritems():
+                value2path(file, "/".join([path, k]), v)
+        else:
+            if type(node) == type(np.array(1)):
+                val = node
+            else:
+                val = node.values
+
+            file.create_dataset("/".join([path, "value"]), data=val)
+
+
+    h5f = h5py.File(file)
+    value2path(h5f, "", X)
+    h5f.close()
 
 def load(file, path="/"):
     def path2value(path):
@@ -22,9 +42,10 @@ def load(file, path="/"):
             if (len(value.shape) == 2):
                 return pd.DataFrame(value.transpose(), index=names[0], columns=names[1])
             else:
+                #TODO: use hierarchical indexing here
                 raise Exception("only 1d/2d arrays supported so far")
         else:
             return _H5Node({n: path2value("/".join([path, n])) for n in fpk})
 
-    file = h5py.File(file, 'r')
-    return path2value(path)
+    with h5py.File(file, 'r') as file:
+        return path2value(path)
