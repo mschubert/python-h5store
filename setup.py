19b4f5307bfa3c4c8fe9ddc82db80dc4c063b2c5
mschubert <mschu.dev@gmail.com>
2015-01-07 14:19:55 +0000

    rename to h5store

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(
-    name='h5shuttle',
+    name='h5store',
     version='0.0.1',
     author='Michael Schubert',
     author_email='mschu.dev+h5@gmail.com',
-    packages=['h5shuttle'],
+    packages=['h5store'],
 #    scripts=['bin/stowe-towels.py'],
 #    url='http://pypi.python.org/pypi/h5shuttle/',
 #    license='LICENSE.txt',
